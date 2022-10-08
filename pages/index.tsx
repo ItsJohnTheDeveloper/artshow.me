dc0fdf36062cf66f1e6927be6f9e8496243d2234
jderbyshire <jderbyshire@acromedia.com>
2022-10-08 16:40:13 -0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <Layout>
       <div className="page">
-        <main>this page is intentionally blank</main>
+        <main>this page is intentionally blank.</main>
       </div>
     </Layout>
   );
