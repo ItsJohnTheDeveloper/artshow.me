2c1703e56bb3738e535c07832e9c0c994bbda110
jderbyshire <jderbyshire@acromedia.com>
2022-10-11 19:39:10 -0700

    added a sample profile link for demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import Layout from "../components/Layout";
 
@@ -6,6 +7,13 @@ const Home = () => {
     <Layout>
       <div className="page">
         <main>this page is intentionally blank.</main>
+        <br />
+        <span>
+          <Link href="/artist/6293dca2d671e0ad7d7878ea">
+            view a sample profile
+          </Link>{" "}
+          or create a new account.
+        </span>
       </div>
     </Layout>
   );
