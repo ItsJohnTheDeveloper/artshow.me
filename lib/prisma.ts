af15b8b0a8e82caad2692d660b60892960443c1d
jderbyshire <jderbyshire@acromedia.com>
2022-10-13 20:55:08 -0700

    removed condition for prod in prisma setup file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,21 +2,22 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    console.log("No cached connection - Prisma Connecting...");
-    try {
-      global.prisma = new PrismaClient();
-    } catch (err) {
-      console.error(err);
-      console.log("Prisma Client can't run right now :(");
-    }
-  }
-  console.log("Using cached connection.");
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
 
-  prisma = global.prisma;
+// } else {
+if (!global.prisma) {
+  console.log("No cached connection - Prisma Connecting...");
+  try {
+    global.prisma = new PrismaClient();
+  } catch (err) {
+    console.error(err);
+    console.log("Prisma Client can't run right now :(");
+  }
 }
+console.log("Using cached connection.");
+
+prisma = global.prisma;
+// }
 
 export default prisma;
