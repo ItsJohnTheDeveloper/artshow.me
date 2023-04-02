1fc92344709d53157985110054ab699998ec42d9
jderbyshire <jderbyshire@acromedia.com>
2023-04-01 21:58:44 -0700

    staging: testing if the route is the issue :shrug-emoji:

---
+++ src/pages/test/[id].tsx
@@ -0,0 +1,24 @@
+import { GetServerSideProps } from "next";
+
+const index = (props) => {
+  console.log(props);
+  return <div>check your console for logs</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const id = params.id as string;
+
+  const artist = await prisma.user.findUnique({
+    where: { id },
+  });
+
+  return {
+    props: {
+      data: {
+        artist: JSON.parse(JSON.stringify(artist)),
+      },
+    },
+  };
+};
+
+export default index;
