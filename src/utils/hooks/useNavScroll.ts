569881abdc17ff9f816e93f20a5bc8a8e0b1c999
jderbyshire <jderbyshire@acromedia.com>
2023-09-09 13:15:30 -0700

    tweaked navbar scroll behavior

--- src/utils/hooks/useNavScroll.ts
+++ src/utils/hooks/useNavScroll.ts
@@ -2,6 +2,7 @@ import { useScrollTrigger } from "@mui/material";
 
 const useNavScroll = () => {
   const config = {
+    disableHysteresis: true,
     // add any config options here
   };
   return useScrollTrigger(config);
